f45dd1dedc74e3003e61248d58d50c773d47b010
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-05-04 19:31:48 +0100

    add facade to generate pdf and delete the use for request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Me;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class HomeController extends Controller
 {
@@ -13,5 +13,10 @@ class HomeController extends Controller
         $info = $Me[0]->informations;
         $informations = json_decode($info);
         return view("index", compact('informations'));
+    }
+    public function generate_pdf()
+    {
+        $filePath = str_replace('\\', '/', public_path('pdf/Ait-Ouaguerd-Anass.pdf'));
+        return Response::download($filePath, 'Ait-Ouaguerd-Anass.pdf');
     }
 }
